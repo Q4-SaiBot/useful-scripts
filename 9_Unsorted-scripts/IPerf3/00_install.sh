59131c504add50ff702d9128125ebc850c64e2b7
Q4-SaiBot@TEST <Q4.SaiBot@GMail.com>
2020-10-29 06:51:10 +0300

    Немного поправил описание и добавил одну команду для проверки скорости с сервером в Москве.

--- 9_Unsorted-scripts/IPerf3/00_install.sh
+++ 9_Unsorted-scripts/IPerf3/00_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #
-#
+#   nload - для того, чтобы визуально глянуть
 #
 sudo apt install iperf3 nload
--- 9_Unsorted-scripts/IPerf3/server.sh
+++ 9_Unsorted-scripts/IPerf3/server.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
+
+################################################################################
 #
-# Запускаем на срвере. Он начинает слушать на 5201 порт.
+#   Запускаем на срвере. Он начинает слушать на 5201 порт.
 #
+################################################################################
+
+
+# Server specific:
+#   -s, --server              run in server mode
+#   -D, --daemon              run the server as a daemon
+#   -I, --pidfile file        write PID file
+#   -1, --one-off             handle one client connection then exit
+#   --rsa-private-key-path    path to the RSA private key used to decrypt
+#                             authentication credentials
+#   --authorized-users-path   path to the configuration file containing user
+#                             credentials
+
+#
+#   Для запуска сервера вполне достаточно такой команды.
+#
+
 iperf3 -s
